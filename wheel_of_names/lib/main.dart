eb5593b57394f3924556003892cdcccfba4021be
Carlo Echon <carloechon1@gmail.com>
2024-05-13 11:55:52 +0800

    Added Suggested Features
    Included the ability to edit the text field in case of typos on the users part. Also added another tab for a different wheel as per the suggestion of friend.

--- wheel_of_names/lib/main.dart
+++ wheel_of_names/lib/main.dart
@@ -26,58 +26,91 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  StreamController<int> selected = StreamController<int>();
+class _MyHomePageState extends State<MyHomePage>{
 
-  final textBoy = TextEditingController();
+  final List<String> chosenNames1 = ["", ""];
+  final List<String> chosenNames2 = ["", ""];
 
-  var chosenNames = <String>[
-    "",
-    "",
-  ];
+  final TextEditingController textBoy1 = TextEditingController();
+  final TextEditingController textBoy2 = TextEditingController();
 
   @override
   void dispose() {
-    textBoy.dispose();
-    selected.close();
+    textBoy1.dispose();
+    textBoy2.dispose();
     super.dispose();
   }
 
-  void commit(){
-      Navigator.of(context).pop();
-    }
-
-  
-
   @override
   Widget build(BuildContext context) {
-  
-    return Scaffold(
+
+    return DefaultTabController(
+      length: 2, 
+      child: Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        bottom: const TabBar(
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.egg),),
+            Tab(icon: Icon(Icons.egg_alt),)
+          ]
+          ),
       ),
+      body: TabBarView(
+        children: <Widget>[
+          Wheel1Page(
+            chosenNames: chosenNames1,
+            textBoy: textBoy1,
+          ),
+          Wheel2Page(
+            chosenNames: chosenNames2,
+            textBoy: textBoy2,
+          ),
+        ]
+           
+        ),
+      )
+    );
+  }
+    
+}
+
+class Wheel1Page extends StatefulWidget {
+  const Wheel1Page({Key? key, required this.chosenNames, required this.textBoy})
+      : super(key: key);
+
+  final List<String> chosenNames;
+  final TextEditingController textBoy;
+
+  @override
+  _Wheel1PageState createState() => _Wheel1PageState();
+}
+
+class _Wheel1PageState extends State<Wheel1Page> {
+  StreamController<int> selected = StreamController<int>();
+
+  @override
+  void dispose() {
+    selected.close();
+    super.dispose();
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body:  GestureDetector(
         onTap: () {
           setState(() {
             selected.add(
-              Fortune.randomInt(0, chosenNames.length),
+              Fortune.randomInt(0, widget.chosenNames.length),
             );
           });
         },
@@ -87,14 +120,14 @@ class _MyHomePageState extends State<MyHomePage> {
               child: FortuneWheel(
                 selected: selected.stream,
                 items: [
-                  for (var it in chosenNames) FortuneItem(child: Text(it)),
+                  for (var it in widget.chosenNames) FortuneItem(child: Text(it)),
                 ],
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton:  Column(
+    floatingActionButton:  Column(
     mainAxisAlignment: MainAxisAlignment.end,
     crossAxisAlignment: CrossAxisAlignment.end,
     children: [
@@ -104,7 +137,9 @@ class _MyHomePageState extends State<MyHomePage> {
           builder: (BuildContext context) => AlertDialog(
             title: const Text('Add a Word'),
             content: TextField(
-              controller: textBoy,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              controller: widget.textBoy,
               autofocus: true,
               decoration: InputDecoration(hintText: 'Enter a word',),
             ),
@@ -115,10 +150,19 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               TextButton(
                 onPressed: () {
-                  chosenNames.remove("");
-                  chosenNames.add(textBoy.text);
-                  print(chosenNames);
-                  textBoy.clear();
+                  widget.chosenNames.clear();
+                  String textInputs = widget.textBoy.text.trim();
+                  List <String> entries = textInputs.split(',');
+                  for (String entry in entries) {
+                    String trimmedEntry = entry.trim();
+                    if (trimmedEntry.isNotEmpty) {
+                      widget.chosenNames.add(trimmedEntry);
+                    }
+                  }
+                  if (widget.chosenNames.length < 2) {
+                    widget.chosenNames.add("");
+                  }
+                  print(widget.chosenNames);
                   setState(() {});
                   Navigator.pop(context, 'OK');
                 },
@@ -133,9 +177,9 @@ class _MyHomePageState extends State<MyHomePage> {
       SizedBox(height: 16), // Adjust as needed for spacing
       FloatingActionButton(
         onPressed: () {
-          chosenNames.clear();
-          chosenNames.add("");
-          chosenNames.add("");
+          widget.chosenNames.clear();
+          widget.chosenNames.add("");
+          widget.chosenNames.add("");
           setState(() {});
         },
         tooltip: 'Second action',
@@ -143,8 +187,118 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     ],
   ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
+
   }
-    
+
+}
+
+class Wheel2Page extends StatefulWidget {
+  const Wheel2Page({Key? key, this.chosenNames = const [], required this.textBoy})
+      : super(key: key);
+
+  final List<String> chosenNames;
+  final TextEditingController textBoy;
+
+  @override
+  _Wheel2PageState createState() => _Wheel2PageState();
+}
+
+class _Wheel2PageState extends State<Wheel2Page> {
+  StreamController<int> selected2 = StreamController<int>();
+
+  @override
+  void dispose() {
+    selected2.close();
+    super.dispose();
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  GestureDetector(
+        onTap: () {
+          setState(() {
+            selected2.add(
+              Fortune.randomInt(0, widget.chosenNames.length),
+            );
+          });
+        },
+        child: Column(
+         children: [
+            Expanded(
+              child: FortuneWheel(
+                selected: selected2.stream,
+                items: [
+                  for (var it in widget.chosenNames) FortuneItem(child: Text(it)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    floatingActionButton:  Column(
+    mainAxisAlignment: MainAxisAlignment.end,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      FloatingActionButton(
+        onPressed: () => showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Add a Word'),
+            content: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              controller: widget.textBoy,
+              autofocus: true,
+              decoration: InputDecoration(hintText: 'Enter a word',),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  widget.chosenNames.clear();
+                  String textInputs = widget.textBoy.text.trim();
+                  List <String> entries = textInputs.split(',');
+                  for (String entry in entries) {
+                    String trimmedEntry = entry.trim();
+                    if (trimmedEntry.isNotEmpty) {
+                      widget.chosenNames.add(trimmedEntry);
+                    }
+                  }
+                  if (widget.chosenNames.length < 2) {
+                    widget.chosenNames.add("");
+                  }
+                  print(widget.chosenNames);
+                  setState(() {});
+                  Navigator.pop(context, 'OK');
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          )
+        ),
+        tooltip: 'Add a word',
+        child: const Icon(Icons.add),
+      ),
+      SizedBox(height: 16), // Adjust as needed for spacing
+      FloatingActionButton(
+        onPressed: () {
+          widget.chosenNames.clear();
+          widget.chosenNames.add("");
+          widget.chosenNames.add("");
+          setState(() {});
+        },
+        tooltip: 'Second action',
+        child: const Icon(Icons.delete),
+      ),
+    ],
+  ),
+    );
+
+  }
+
 }
